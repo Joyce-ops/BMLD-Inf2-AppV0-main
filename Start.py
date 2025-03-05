8ef602a05a3c9ed8353bd52f8a859ca35e476f0d
Joyce Baumann <baumajoy@students.zhaw.ch>
2025-03-05 13:34:58 +0100

    test

--- Start.py
+++ Start.py
@@ -3,8 +3,9 @@ import pandas as pd
 
 st.title("Stoffmengenrechner")
 st.write("Hier kannst du per Eingabe von Masse und Molare Masse die Stoffmenge berechnen.")
-if st.button("Rechner Stoffmenge"):
+if st.button("Stoffmengen Rechner"):
     st.switch_page("pages/1_Rechner.py")
+
 # !! WICHTIG: Eure Emails müssen in der App erscheinen!!
 
 # Streamlit über den Text unten direkt in die App - cool!
@@ -14,4 +15,3 @@ Diese App wurde von folgenden Personen entwickelt:
 - Mcqulat Miller (millemcq@students.zhaw.ch)
 
 """
-
