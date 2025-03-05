97b507985d59f0c48bcb76ece7119ab5c4e0c7a1
Joyce Baumann <baumajoy@students.zhaw.ch>
2025-03-05 10:14:35 +0100

    gehts?

--- pages/1_Rechner.py
+++ pages/1_Rechner.py
@@ -4,14 +4,13 @@ st.title("Rechner")
 
 st.header("Berechnung von Stoffmengen")
 st.number_input("Masse in g", 0.0)
-st.number_input("Molare Masse in g/mol", 0.0)
+st.number_input("Molare Masse in g/mol", 0.000)
 st.write("Ergebnis: Stoffmenge in mol")
 
 submitted = st.form_submit_button("Submit")
     
 if submitted:
-    
-    result = calculate_stoffmenge(Masse, Molare Masse)
+        result = calculate_stoffmenge(m, g/mol)
     
     st.write(f'Die Stoffmenge ist: {result["Stoffmenge"]} mol')
     st.write(f'Berechnet am: {result["timestamp"].strftime("%d.%m.%Y %H:%M:%S")}')
