966d9bfcc7f5a27697b084e902e1f24d10a1fba0
Joyce Baumann <baumajoy@students.zhaw.ch>
2025-03-05 10:39:32 +0100

    update

--- pages/1_Rechner.py
+++ pages/1_Rechner.py
@@ -1,11 +1,27 @@
 import streamlit as st
 
-st.title("Rechner")
+st.title("Stoffmengenrechner")
 
 st.header("Berechnung von Stoffmengen")
-st.number_input("Masse in g", 0.0)
-st.number_input("Molare Masse in g/mol", 0.000)
-st.write("Ergebnis: Stoffmenge in mol")
+
+
+def calculate_Stoffmenge(Masse, Molaremasse):
+    Stoffmenge = Masse / Molaremasse
+
+    return Stoffmenge
+    
+
+with st.form("Stoffmenge berechner"):
+    Masse = st.number_input('Geben Sie die Masse ein (in Gramm)', min_value=0.0001, max_value=10000.0, value=10.0, step=0.0001)
+    Molaremasse = st.number_input('Geben Sie die Molaremasse ein (in g/mol)', min_value=1.0, max_value=500.0, value=1.008, step=0.0001)
+
+    submitted = st.form_submit_button("Submit")
+    
+if submitted:
+    
+    result = calculate_Stoffmenge(Masse, Molaremasse)
+    
+    st.write(f'Die Stoffmenge ist: {result}')
 
 if st.button("Home"):
     st.switch_page("Start.py")
