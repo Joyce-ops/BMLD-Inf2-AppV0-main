a6eb0246692a274043ff92a87b3dba5d2be55c68
Joyce Baumann <baumajoy@students.zhaw.ch>
2025-03-05 10:24:12 +0100

    versuch

--- pages/2_Unterseite.py
+++ pages/2_Unterseite.py
@@ -7,12 +7,14 @@ st.write("Diese Seite ist eine Unterseite der Startseite.")
 if st.button("Home"):
     st.switch_page("Start.py")
 
+def calculate_Stoffmenge(Masse, Molaremasse):
+    Stoffmenge = Masse / Molaremasse
+    return {"Stoffmenge": Stoffmenge, "timestamp": datetime.now()}
+
 with st.form("Stoffmenge berechner"):
-    # Get user input for height and weight
     Masse = st.number_input('Geben Sie die Masse ein (in Gramm)', min_value=0.001, max_value=10000.0, value=1.7, step=0.01)
     Molaremasse = st.number_input('Geben Sie die Molaremasse ein (in g/mol)', min_value=1.0, max_value=500.0, value=70.0, step=0.1)
 
-    # Every form must have a submit button.
     submitted = st.form_submit_button("Submit")
     
 if submitted:
@@ -21,4 +23,3 @@ if submitted:
     
     st.write(f'Die Stoffmenge ist: {result["Stoffmenge"]}')
     st.write(f'Berechnet am: {result["timestamp"].strftime("%d.%m.%Y %H:%M:%S")}')
-    st.write(f'Kategorie: {result["category"]}')
