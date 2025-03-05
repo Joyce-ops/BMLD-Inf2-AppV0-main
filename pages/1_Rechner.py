02573610e3b281b1dac6e9ca728d0343c2670fcd
Joyce Baumann <baumajoy@students.zhaw.ch>
2025-03-05 09:13:41 +0100

    test

--- pages/1_Rechner.py
+++ pages/1_Rechner.py
@@ -3,6 +3,14 @@ import streamlit as st
 st.title("1_Rechner")
 
 st.write("Berechnung von Stoffmengen")
+st.number_input("Masse in g", 0.0)
+st.number_input("Molare Masse in g/mol", 0.0)
+if molare_masse != 0:
+    stoffmenge = masse / molare_masse
+    st.write(f"Stoffmenge: {stoffmenge} mol")
+else:
+    st.write("Bitte gib eine gültige molare Masse ein.")
+
 
 if st.button("Home"):
     st.switch_page("Start.py")
