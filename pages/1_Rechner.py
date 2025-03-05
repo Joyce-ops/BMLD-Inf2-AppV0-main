9f7053e744fb4fb76c305d163f31718bfb257b2b
Joyce Baumann <baumajoy@students.zhaw.ch>
2025-03-05 10:20:22 +0100

    testversuch

--- pages/1_Rechner.py
+++ pages/1_Rechner.py
@@ -7,8 +7,5 @@ st.number_input("Masse in g", 0.0)
 st.number_input("Molare Masse in g/mol", 0.000)
 st.write("Ergebnis: Stoffmenge in mol")
 
-submitted = st.form_submit_button("Submit")
-
-
 if st.button("Home"):
     st.switch_page("Start.py")
--- pages/2_Unterseite.py
+++ pages/2_Unterseite.py
@@ -7,18 +7,18 @@ st.write("Diese Seite ist eine Unterseite der Startseite.")
 if st.button("Home"):
     st.switch_page("Start.py")
 
-with st.form("BMI Eingabeformular"):
+with st.form("Stoffmenge berechner"):
     # Get user input for height and weight
-    height = st.number_input('Geben Sie Ihre Größe ein (in Meter)', min_value=0.1, max_value=3.0, value=1.7, step=0.01)
-    weight = st.number_input('Geben Sie Ihr Gewicht ein (in kg)', min_value=1.0, max_value=500.0, value=70.0, step=0.1)
+    Masse = st.number_input('Geben Sie die Masse ein (in Gramm)', min_value=0.001, max_value=10000.0, value=1.7, step=0.01)
+    Molaremasse = st.number_input('Geben Sie die Molaremasse ein (in g/mol)', min_value=1.0, max_value=500.0, value=70.0, step=0.1)
 
     # Every form must have a submit button.
     submitted = st.form_submit_button("Submit")
     
 if submitted:
     
-    result = calculate_bmi(height, weight)
+    result = calculate_Stoffmenge(Masse, Molaremasse)
     
-    st.write(f'Ihr BMI ist: {result["bmi"]}')
+    st.write(f'Die Stoffmenge ist: {result["Stoffmenge"]}')
     st.write(f'Berechnet am: {result["timestamp"].strftime("%d.%m.%Y %H:%M:%S")}')
     st.write(f'Kategorie: {result["category"]}')
