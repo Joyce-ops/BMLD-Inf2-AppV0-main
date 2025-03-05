7b132b51fcdb361ccaa43db1b86f36c836d1539d
Joyce Baumann <baumajoy@students.zhaw.ch>
2025-03-05 21:13:58 +0100

    periodensystem hinzugefügt

--- Start.py
+++ Start.py
@@ -6,12 +6,15 @@ st.write("Hier kannst du per Eingabe von Masse und Molare Masse die Stoffmenge b
 if st.button("Hier gehts zum Rechner"):
     st.switch_page("pages/1_Rechner.py")
 
-# !! WICHTIG: Eure Emails müssen in der App erscheinen!!
-
-# Streamlit über den Text unten direkt in die App - cool!
 """
 Diese App wurde von folgenden Personen entwickelt:
 - Joyce Baumann (Baumajoy@students.zhaw.ch)
 - Mcqulat Miller (millemcq@students.zhaw.ch)
 
 """
+image_url = "https://cdn.pixabay.com/photo/2016/08/28/16/26/periodic-table-1626299_1280.png"
+st.image(image_url, caption="Periodensystem", use_container_width=True)
+
+# !! WICHTIG: Eure Emails müssen in der App erscheinen!!
+
+# Streamlit über den Text unten direkt in die App - cool!
