42c5f2d7138f2e839e44bbdbefa11466e1badd41
Joyce Baumann <baumajoy@students.zhaw.ch>
2025-03-05 09:31:30 +0100

    update2

--- Start.py
+++ Start.py
@@ -3,6 +3,9 @@ import pandas as pd
 
 st.title("App von Joyce und Mcqulat")
 st.write("Willkommen zur App von Joyce und Mcqulat. Hier kannst du einen Rechner finden, die dir im Chemie-Alltag helfen können.")
+
+if st.button("Rechner Stoffmenge"):
+    st.switch_page("pages/1_Rechner.py")
 # !! WICHTIG: Eure Emails müssen in der App erscheinen!!
 
 # Streamlit über den Text unten direkt in die App - cool!
@@ -15,5 +18,4 @@ Autoren:
 - Joyce Baumann (Baumajoy@students.zhaw.ch)
 - Mcqulat Miller (millemcq@students.zhaw.ch)
 """
-if st.button("Rechner Stoffmenge"):
-    st.switch_page("pages/1_Rechner.py")
+
