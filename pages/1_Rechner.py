2bd44e908fe169ad514e68fabc9ed2ab5d3658ad
Joyce Baumann <baumajoy@students.zhaw.ch>
2025-03-05 09:14:35 +0100

    test

--- pages/1_Rechner.py
+++ pages/1_Rechner.py
@@ -5,11 +5,8 @@ st.title("1_Rechner")
 st.write("Berechnung von Stoffmengen")
 st.number_input("Masse in g", 0.0)
 st.number_input("Molare Masse in g/mol", 0.0)
-if molare_masse != 0:
-    stoffmenge = masse / molare_masse
-    st.write(f"Stoffmenge: {stoffmenge} mol")
-else:
-    st.write("Bitte gib eine gültige molare Masse ein.")
+st.write("Stoffmenge in mol")
+st.write("Ergebnis")
 
 
 if st.button("Home"):
