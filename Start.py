02bf1cd4de42a0d5d83aa3fa24ee947bff6d40fb
Joyce Baumann <baumajoy@students.zhaw.ch>
2025-03-05 09:19:06 +0100

    update

--- Start.py
+++ Start.py
@@ -15,3 +15,5 @@ Autoren:
 - Joyce Baumann (Baumajoy@students.zhaw.ch)
 - Mcqulat Miller (millemcq@students.zhaw.ch)
 """
+if st.button("Rechner"):
+    st.switch_page("pages/1_Rechner.py")
--- pages/1_Rechner.py
+++ pages/1_Rechner.py
@@ -1,15 +1,12 @@
 import streamlit as st
 
-st.title("1_Rechner")
+st.title("Rechner")
 
-st.write("Berechnung von Stoffmengen")
+st.header("Berechnung von Stoffmengen")
 st.number_input("Masse in g", 0.0)
 st.number_input("Molare Masse in g/mol", 0.0)
-st.write("Stoffmenge in mol")
-st.write("Ergebnis")
+st.write("Ergebnis: Stoffmenge in mol")
 
 
 if st.button("Home"):
     st.switch_page("Start.py")
-if st.button("Rechner"):
-    st.switch_page("pages/1_Rechner.py")
